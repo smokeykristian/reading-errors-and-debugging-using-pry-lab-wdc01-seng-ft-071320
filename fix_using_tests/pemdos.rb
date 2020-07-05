a1234d07fb8ae6504cfb19dfabdc359221dd0d09
Kristian Portillo <portillk@gmail.com>
2020-07-05 20:31:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  
+  if string[0] = "s"
+    
+   print ("s" * 10) + string
+      binding.pry
+  end
+    
+    
   else
-    string
+   p string
   end
 end
